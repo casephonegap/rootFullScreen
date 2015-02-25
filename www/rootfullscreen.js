f0e267b5cf9ea80bc444da60d64177d3249fae25
casephonegap <office@case.at>
2015-02-25 10:10:28 +0100

    Update rootfullscreen.js

--- www/rootfullscreen.js
+++ www/rootfullscreen.js
@@ -2,16 +2,20 @@ function rootFullScreen() { this.fsOnFlag = false; };
 
 /* hide android software buttons */
 rootFullScreen.prototype.enable = function () 
-{ if (this.fsOnFlag == false) 
+{ console.log('rootFullScreen.enable aufgerufen');
+  if (this.fsOnFlag == false) 
   { this.fsOnFlag = true;
+    console.log('enable cordova aufruf absetzen');
     cordova.exec(null, null, 'rootFullScreen', 'enable', []);
   }
 };
 
 /* show android software buttons */
 rootFullScreen.prototype.disable = function () 
-{ if (this.fsOnFlag == true) 
+{ console.log('rootFullScreen.disable aufgerufen');
+  if (this.fsOnFlag == true) 
   { this.fsOnFlag = false;
+    console.log('disable cordova aufruf absetzen');
     cordova.exec(null, null, 'rootFullScreen', 'disable', []);
   }
 };
